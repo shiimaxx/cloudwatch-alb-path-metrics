1cb3dc61d9f7e9045cd6e7850a28360bcb1a83ea
Takatada Yoshima <yoshima@fulldrive.jp>
2025-09-01 22:06:54 +0900

    wip

--- main.go
+++ main.go
@@ -20,6 +20,7 @@ import (
 	"github.com/aws/aws-sdk-go-v2/service/cloudwatch"
 	"github.com/aws/aws-sdk-go-v2/service/cloudwatch/types"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
+
 	"github.com/shiimaxx/cloudwatch-alb-path-metrics/internal/matcher"
 )
 
