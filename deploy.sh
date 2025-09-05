506a7355315fd1abbd7b599128187a61f4fab4e0
Takatada Yoshima <yoshima@fulldrive.jp>
2025-09-05 20:42:59 +0900

    wip

--- deploy.sh
+++ deploy.sh
@@ -4,10 +4,11 @@ GOOS=linux GOARCH=arm64 go build -tags lambda.norpc -o dist/bootstrap main.go
 cd dist
 zip cloudwatch-alb-path-metrics.zip bootstrap
 aws lambda update-function-code \
-  --no-paginate \
+  --no-cli-pager \
   --function-name cloudwatch-alb-path-metrics \
   --zip-file fileb://cloudwatch-alb-path-metrics.zip
+sleep 3
 aws lambda update-function-configuration \
-  --no-paginate \
+  --no-cli-pager \
   --function-name cloudwatch-alb-path-metrics \
   --environment Variables="{SERVICE=hello,FILTER='method == \"POST\" && path matches \"/graphql\"',GROUPS='^/graphql$'}"
