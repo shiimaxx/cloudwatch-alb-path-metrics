28ed6669d66f4fbaa18fba1a4facc9b202bbb8c0
Takatada Yoshima <yoshima@fulldrive.jp>
2025-09-26 17:28:30 +0900

    wip

--- go.mod
+++ go.mod
@@ -5,13 +5,29 @@ go 1.24.1
 require github.com/aws/aws-lambda-go v1.49.0
 
 require (
-	github.com/aws/aws-sdk-go-v2 v1.39.0
+	github.com/aws/aws-sdk-go-v2 v1.39.1
 	github.com/aws/aws-sdk-go-v2/service/cloudwatch v1.50.1
 	github.com/expr-lang/expr v1.17.6
 	github.com/stretchr/testify v1.7.2
 )
 
 require (
+	github.com/aws/aws-sdk-go-v2/aws/protocol/eventstream v1.7.1 // indirect
+	github.com/aws/aws-sdk-go-v2/config v1.31.10 // indirect
+	github.com/aws/aws-sdk-go-v2/credentials v1.18.14 // indirect
+	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.18.8 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/configsources v1.4.8 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.7.8 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/ini v1.8.3 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/v4a v1.4.8 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/accept-encoding v1.13.1 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/checksum v1.8.8 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.13.8 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/s3shared v1.19.8 // indirect
+	github.com/aws/aws-sdk-go-v2/service/s3 v1.88.2 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sso v1.29.4 // indirect
+	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.35.0 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sts v1.38.5 // indirect
 	github.com/aws/smithy-go v1.23.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
--- main.go
+++ main.go
@@ -1,13 +1,68 @@
 package main
 
 import (
+	"bufio"
+	"compress/gzip"
 	"context"
+	"fmt"
+	"net/url"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
 )
 
 func handler(ctx context.Context, s3Event events.S3Event) error {
+	cfg, err := config.LoadDefaultConfig(ctx)
+	if err != nil {
+		return fmt.Errorf("load AWS config: %w", err)
+	}
+
+	s3Client := s3.NewFromConfig(cfg)
+
+	for _, record := range s3Event.Records {
+		bucket := record.S3.Bucket.Name
+		if bucket == "" {
+			return fmt.Errorf("missing bucket name in S3 event record")
+		}
+
+		key, err := url.QueryUnescape(record.S3.Object.Key)
+		if err != nil {
+			return fmt.Errorf("decode object key %q: %w", record.S3.Object.Key, err)
+		}
+
+		if err := streamObjectLines(ctx, s3Client, bucket, key); err != nil {
+			return fmt.Errorf("stream s3://%s/%s: %w", bucket, key, err)
+		}
+	}
+
+	return nil
+}
+
+func streamObjectLines(ctx context.Context, client *s3.Client, bucket, key string) error {
+	resp, err := client.GetObject(ctx, &s3.GetObjectInput{Bucket: aws.String(bucket), Key: aws.String(key)})
+	if err != nil {
+		return fmt.Errorf("get object: %w", err)
+	}
+	defer resp.Body.Close()
+
+	gzipReader, err := gzip.NewReader(resp.Body)
+	if err != nil {
+		return fmt.Errorf("create gzip reader: %w", err)
+	}
+	defer gzipReader.Close()
+
+	scanner := bufio.NewScanner(gzipReader)
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		return fmt.Errorf("scan gzip stream: %w", err)
+	}
+
 	return nil
 }
 
