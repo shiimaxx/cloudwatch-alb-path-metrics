da98c67f922fce498e277ac48be4699918190fbd
Takatada Yoshima <yoshima@fulldrive.jp>
2025-09-29 11:21:29 +0900

    wip

--- scripts/invoke.sh
+++ scripts/invoke.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-curl http://localhost:9000/2015-03-31/functions/function/invocations -d '
+curl http://localhost:9000/2015-03-31/functions/function/invocations -d "
 {
-  "Records":
+  \"Records\":
   [
-    {"s3":{"bucket":{"name":"hello"}},"object":{"key":"hoge"}}
+    {\"s3\":{\"bucket\":{\"name\":\"${BUCKET}\"}},\"object\":{\"key\":\"${KEY}\"}}
   ]
-}'
+}"
